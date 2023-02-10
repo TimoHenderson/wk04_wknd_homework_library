1733312e7bdbe3dc5bb69c2ef5db7ba7a234da54
TimoHenderson <tjhenderson2@gmail.com>
2023-02-10 12:01:17 +0000

    added Library.add_book

--- models/library.py
+++ models/library.py
@@ -1,3 +1,10 @@
+from models.book import Book
+
+
 class Library:
     def __init__(self, book_list=[]):
         self.book_list = book_list
+
+    def add_book(self, title, author, genre):
+        new_book = Book(title, author, genre)
+        self.book_list.append(new_book)
--- tests/library_test.py
+++ tests/library_test.py
@@ -1,12 +1,39 @@
 import unittest
 from models.library import Library
+from models.book import Book
 
 
 class TestLibrary(unittest.TestCase):
     def setUp(self):
         self.empty_library = Library()
 
+        self.book_list = [
+            Book("Popular Music Culture", "Roy Shuker", "Music"),
+            Book("Sound Design", "David Sonnenschein", "Audio"),
+            Book("The Hobbit", "JRR Tolkien", "Fantasy"),
+        ]
+
+        self.full_library = Library(self.book_list)
+
     def test_has_book_list__empty(self):
         actual = self.empty_library.book_list
         expected = []
         self.assertEqual(actual, expected)
+
+    def test_has_book_list__not_empty(self):
+        actual = self.full_library.book_list
+        expected = self.book_list
+        self.assertEqual(actual, expected)
+
+    def test_can_add_book(self):
+        self.full_library.add_book("The Gruffalo", "Julia Donaldson", "Children")
+        actual_book = self.full_library.book_list[-1]
+        actual_title = actual_book.title
+        expected_title = "The Gruffalo"
+        self.assertEqual(actual_title, expected_title)
+        actual_author = actual_book.author
+        expected_author = "Julia Donaldson"
+        self.assertEqual(actual_author, expected_author)
+        actual_genre = actual_book.genre
+        expected_genre = "Children"
+        self.assertEqual(actual_genre, expected_genre)
