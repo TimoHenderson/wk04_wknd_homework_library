952ca8c7d0e8afba9e3705b2832106500b7f5466
TimoHenderson <tjhenderson2@gmail.com>
2023-02-10 10:59:50 +0000

    Added Book.author

--- tests/book_test.py
+++ tests/book_test.py
@@ -4,9 +4,19 @@ from models.book import Book
 
 class TestBook(unittest.TestCase):
     def setUp(self):
-        self.book_1 = Book("Sound Design")
+        self.book_1 = Book("Sound Design", "David Sonnenschein")
 
     def test_has_title(self):
         actual = self.book_1.title
         expected = "Sound Design"
+        self.assertEqual(actual, expected)
+
+    def test_has_author(self):
+        actual = self.book_1.author
+        expected = "David Sonnenschein"
+        self.assertEqual(actual, expected)
+
+    def test_has_genre(self):
+        actual = self.book_1.genre
+        expected = "Audio"
         self.assertEqual(actual, expected)
